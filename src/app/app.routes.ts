76e4c3f378f06fa4a247432b2cc38eee91901ac0
Marcos <marcosturossi@gmail.com>
2026-08-14 13:46:12 -0300

    more improviments

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import {Routes} from '@angular/router';
 import {PageNotFoundComponent} from './pages/errors/page-not-found/page-not-found.component';
+import {ForbiddenComponent} from './pages/errors/forbidden/forbidden.component';
 import { AuthGuard } from './guard/auth.guard';
 
 export const routes: Routes = [
@@ -10,6 +11,7 @@ export const routes: Routes = [
   },
   { path: '',   redirectTo: 'system', pathMatch: 'full' },
   { path: '404', component: PageNotFoundComponent },
+  { path: 'forbidden', component: ForbiddenComponent },
   { path: '**',  redirectTo: '/404' },
 ];
 
--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,7 +1,8 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { createAuthGuard, AuthGuardData } from 'keycloak-angular';
 import Keycloak from 'keycloak-js';
+import { AuthServiceService } from '../services/auth-service.service';
 
 const isAccessAllowed = async (
   _route: ActivatedRouteSnapshot,
@@ -12,6 +13,12 @@ const isAccessAllowed = async (
     await inject(Keycloak).login({ redirectUri: window.location.origin + '/system' });
     return false;
   }
+
+  if (!inject(AuthServiceService).hasAnyRole(['manager', 'admin'])) {
+    inject(Router).navigate(['/forbidden']);
+    return false;
+  }
+
   return true;
 };
 
